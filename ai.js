134b813a789e5afc13f74191a7469ae1736dd35c
Xavier Harris <xharris1@umbc.edu>
2018-10-24 01:44:16 -0400

    added gaussian elimination

--- ai.js
+++ ai.js
@@ -1,22 +1,17 @@
 
-$('<input type="button" value="Run AI" id="ai" style="margin-left:10px;" />' ).insertAfter($('#display-link')[0]);
-let runningButton = $("#ai")[0];
 
-runningButton.addEventListener("click", function() {
-    runAI();
-});
 
-let size = []; // [width, height]
+var size = []; // [width, height]
 
 function getCellFromId(id) {
-    let position = id.split('_');
+    let position = id.replace("#","").split('_');
     position = [parseInt(position[1])-1, parseInt(position[0])-1];
     return getCell(position[0], position[1]);
 }
 
 function getCell(x, y) {
     let index = x + size[0] * y
-    if (index < 0 || index >= cells.length)
+    if (x < 0 || y < 0 || index < 0 || index >= cells.length)
         return;
 
     let class_val = cells[index].classList[1];
@@ -35,19 +30,21 @@ function clickCell(x, y, button) {
         element.dispatchEvent(new MouseEvent("mousedown", {'view':window, 'bubbles':true, 'cancelable':true, 'button':button}));
         element.dispatchEvent(new MouseEvent("mouseup", {'view':window, 'bubbles':true, 'cancelable':true, 'button':button}));
     }
+    scanBoard();
 }
 
-let cells = [];
-let border_cells = {};
-let mines_left = 0;
+var cells = [];
+var border_cells = [];
+var mines_left = 0;
+var open_cells = {};    // { cell_id : [border_cells indexes] }
 function scanBoard() {
     cells = $("#game .square"); 
-    border_cells = {};
+    border_cells = [];
 
     // find border cells
-    let open_cells = $(".square.open1, .square.open2, .square.open3, .square.open4");
+    let _open_cells = $(".square.open1, .square.open2, .square.open3, .square.open4");
 
-    open_cells.each(function(i, e){
+    _open_cells.each(function(i, e){
         // check all neighboring cells
         let open_cell = getCellFromId(e.id);
 
@@ -57,18 +54,16 @@ function scanBoard() {
 
                 // add untouched cell to collection and add the open cells value to it's 'potential'
                 if (cell && cell.value == -1) {
-                    if (!(cell.id in border_cells)) {
-                        border_cells[cell.id] = cell;
-                        border_cells[cell.id].potential = [];
-                        cell.element.addClass("has-potential");
-                    }
+                    if (!(open_cell.id in open_cells))
+                        open_cells[open_cell.id] = [];
 
-                    if (!border_cells[cell.id].potential.includes(open_cell.value)) {
-                        border_cells[cell.id].potential.push(open_cell.value);
+                    let el_index = border_cells.findIndex((element) => element.id == cell.id);
+                    if (el_index < 0) {
+                        border_cells.push(cell);
+                        el_index = border_cells.length - 1;
                     }
-
-                    // get a sum of all neighboring cells ignoring any repeating values
-                    cell.element.html(border_cells[cell.id].potential.reduce((a, b) => a+b, 0))
+                    if (!open_cells[open_cell.id].includes(el_index))
+                        open_cells[open_cell.id].push(el_index);
                 }
             }
         }
@@ -79,18 +74,70 @@ function prepareAI() {
     size = [$("#game .bordertb").length/3, $("#game .borderlr").length/2];
     scanBoard();
     
-    document.addEventListener("click",function(e){
-        scanBoard();
-    });
-    
 }
 
+/*
+Set the maximum bound and minimum bound to zero
+For each column in the row (not including the augmented column of course) if the number is positive add it to the maximum bound and if it is negative then add it to the minimum bound.
+If the augmented column value is equal to the minimum bound then
+   All of the negative numbers in that row are mines and all of the positive values in that row are not mines
+else if the augmented column value is equal to the maximum bound then
+   All of the negative numbers in that row are not mines and all of the positive values in that row are mines.
+Finishing the Simple Example
+*/
+
 function makeMove() {
-    clickCell(0,0,0);
+    let mine_matrix = [];
+    let end_vals = [];
+    for (let cell_id in open_cells) {
+        let b_cell_indexes = open_cells[cell_id];
+        let new_array = [];
+        new_array.length = border_cells.length + 1;
+        new_array.fill(0.0);
+
+        for (let b = 0; b < b_cell_indexes.length; b++) {
+            new_array[b_cell_indexes[b]] = 1.0;
+        }
+        new_array[new_array.length - 1] = getCellFromId(cell_id).value
+        mine_matrix.push(new_array);
+    }
+
+    console.log(JSON.parse(JSON.stringify(mine_matrix)))
+    let g_matrix = gauss(mine_matrix);
+    console.log(g_matrix);
+
+    let suspects = [];
+    for (let r = 0; r < g_matrix.length; r++) {
+        let last_index = -1;
+        let changes = 0;
+        for (let c = 0; c < g_matrix[r].length - 1; c++) {
+            if (g_matrix[r][c] > 0) {
+                last_index = c;
+                changes++;
+            }
+        }
+
+        if (changes == 1 && last_index != -1) {
+            suspects.push(last_index);
+        }
+    }
+
+    // label the suspected spaces
+    for (let s = 0; s < suspects.length; s++) {
+        let cell = border_cells[suspects[s]]; 
+        clickCell(cell.position[0], cell.position[1], 2);
+    }
 }
 
 function runAI() {
+    clickCell(0,0,0);
     makeMove();
+    
 }
 
-prepareAI();
+// "Run AI" button
+$('<input type="button" value="Run AI" id="ai" style="margin-left:10px;" />' ).insertAfter($('#display-link')[0]);
+$("#ai").on("click", function() {
+    prepareAI();
+    runAI();
+});
---
+++ extra.js
@@ -0,0 +1,60 @@
+let abs = Math.abs;
+
+function gauss(A) {
+    function getValue(r, c) { return A[r][c]; }
+
+     // n => totalColumns
+     let totalColumns = A[0].length;
+     // m => totalRows
+     let totalRows = A.length;
+
+     // i => row
+     let row = 0;
+     // j => col
+     let col = 0;
+     while ((row < totalRows) && (col < totalColumns)) {
+        let max_row = row;
+        // k => currentRow
+        // (*log) << "Current row: " << col << logging::endl;
+        for (let currentRow = row + 1; currentRow < totalRows; ++currentRow) {
+           if (abs(getValue(currentRow, col)) > abs(getValue(max_row, col))) {
+              max_row = currentRow;
+           }
+        }
+        
+        if (getValue(max_row, col) != 0.0)
+        {
+           // Swap the rows around. Put it in its own scope to remove the temp
+           // variable asap.
+           if(row != max_row)
+           {
+              let temp = A[row]; 
+              A[row] = A[max_row]; 
+              A[max_row] = temp;
+              // (*log) << "Swapped rows " << row << " and " << max_row << logging::endl;
+           }
+
+           let currentValue = getValue(row, col);
+           A[row] = A[row].map((x) => x * (1.0 / currentValue));
+
+           // u => iterRow
+           for(let iterRow = row + 1; iterRow < totalRows; ++iterRow)
+           {
+              let mulVal = -getValue(iterRow, col);
+              if(mulVal != 0.0)
+              {
+                 A[row] = A[row].map((x) => x * mulVal);
+                 A[iterRow] = A[iterRow].map((x, i) => x + A[row][i]);
+                 A[row] = A[row].map((x => x * (1.0 / mulVal)));
+                 // (*log) << "Using row " << row << " to fix " << iterRow << logging::endl; 
+                 // render();
+              }
+           }
+
+           row++;
+        }
+
+        col++;
+     }
+     return A;
+}

ffff8e1fd7d3e5f9242b372e08a43146ac15aa98
Xavier Harris <xharris1@umbc.edu>
2018-10-23 20:09:04 -0400

    basic board scanning function impelemented

--- ai.js
+++ ai.js
@@ -6,17 +6,26 @@ runningButton.addEventListener("click", function() {
     runAI();
 });
 
-let cells = [];
 let size = []; // [width, height]
 
+function getCellFromId(id) {
+    let position = id.split('_');
+    position = [parseInt(position[1])-1, parseInt(position[0])-1];
+    return getCell(position[0], position[1]);
+}
+
 function getCell(x, y) {
-    let class_val = cells[x + size[0] * y].classList[1];
-    class_val = class_val.replace("open","");
-    class_val = class_val.replace("blank","-");
+    let index = x + size[0] * y
+    if (index < 0 || index >= cells.length)
+        return;
+
+    let class_val = cells[index].classList[1];
+    class_val = parseInt(class_val.replace("open","").replace("blank","-1"));
     return {
-        value: class_val,
+        value: class_val, // -1 : unclicked, 0 : no number, 1 - 3 : number
         element: $("#"+(y+1)+"_"+(x+1)),
-        id: "#"+(y+1)+"_"+(x+1)
+        id: "#"+(y+1)+"_"+(x+1),
+        position: [x, y]
      };
 }
 
@@ -28,21 +37,60 @@ function clickCell(x, y, button) {
     }
 }
 
+let cells = [];
+let border_cells = {};
+let mines_left = 0;
+function scanBoard() {
+    cells = $("#game .square"); 
+    border_cells = {};
+
+    // find border cells
+    let open_cells = $(".square.open1, .square.open2, .square.open3, .square.open4");
+
+    open_cells.each(function(i, e){
+        // check all neighboring cells
+        let open_cell = getCellFromId(e.id);
+
+        for (let offx = -1; offx <= 1; offx++) {
+            for (let offy = -1; offy <= 1; offy++) {
+                let cell = getCell(open_cell.position[0]+offx, open_cell.position[1]+offy);
+
+                // add untouched cell to collection and add the open cells value to it's 'potential'
+                if (cell && cell.value == -1) {
+                    if (!(cell.id in border_cells)) {
+                        border_cells[cell.id] = cell;
+                        border_cells[cell.id].potential = [];
+                        cell.element.addClass("has-potential");
+                    }
+
+                    if (!border_cells[cell.id].potential.includes(open_cell.value)) {
+                        border_cells[cell.id].potential.push(open_cell.value);
+                    }
+
+                    // get a sum of all neighboring cells ignoring any repeating values
+                    cell.element.html(border_cells[cell.id].potential.reduce((a, b) => a+b, 0))
+                }
+            }
+        }
+    });
+}
+
 function prepareAI() {
     size = [$("#game .bordertb").length/3, $("#game .borderlr").length/2];
-    cells = $("#game .square"); 
+    scanBoard();
     
     document.addEventListener("click",function(e){
-        cells = $("#game .square"); 
+        scanBoard();
     });
-    
     
 }
 
+function makeMove() {
+    clickCell(0,0,0);
+}
+
 function runAI() {
-    for (let i = 0; i < size[0]; i++) {
-        clickCell(i,0,0);
-    }
+    makeMove();
 }
 
 prepareAI();
